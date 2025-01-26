f053f486b5a1b2d259cd72cee220f0f4985f5504
Tanya Davis <tdavis_la@outlook.com>
2025-01-25 21:33:32 -0500

    Inital commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Subpoena Tracker")
+st.header("Log and track the issuance and compliance of subpoenas.")
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+ReadMe Placeholder 
+This project is a subpoena tracker to log and track the issuance and compliance of subpoenas.
