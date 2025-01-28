4b186afb3b4506d0867a7ce3c5582a6fcf9992eb
Tanya Davis <tdavis_la@outlook.com>
2025-01-27 21:43:24 -0500

    Added beginning layout and field inputs

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title("Subpoena Tracker")
-st.header("Log and track the issuance and compliance of subpoenas.")
+st.subheader("Log and track the issuance and compliance of subpoenas.")
 
+# Adding basic subpoena information
+st.write("### Subpoena Data")
+col1, col2, col3 = st.columns(3)
+subpoena_number = col1.text_input("Enter Subpoena Number")
+subpoena_jurisdiction = col2.text_input("Enter Issuing Jurisdiction")
+subpoena_due_date = col3.date_input("Enter Subpoena Due Date", format="MM/DD/YYYY")
