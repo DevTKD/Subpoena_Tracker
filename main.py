dc4555b08d9c687d32b5e93ba3ebe30fb24a1f40
Tanya Davis <tdavis_la@outlook.com>
2025-01-28 08:33:46 -0500

    Added additional data input fields

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ st.title("Subpoena Tracker")
 st.subheader("Log and track the issuance and compliance of subpoenas.")
 
 # Adding basic subpoena information
-st.write("### Subpoena Data")
+st.write("### Basic Subpoena Details")
 col1, col2, col3 = st.columns(3)
-subpoena_number = col1.text_input("Enter Subpoena Number")
-subpoena_jurisdiction = col2.text_input("Enter Issuing Jurisdiction")
-subpoena_due_date = col3.date_input("Enter Subpoena Due Date", format="MM/DD/YYYY")
+subpoena_ID = col1.text_input("Enter Subpoena Number")
+case_name = col2. text_input("Enter Case Name/Number")
+issued_date = col3.date_input("Enter Date Issued", format="MM/DD/YYYY")
+
+#Contuning next set of basic data
+col4, col5 = st.columns(2)
+response_deadline = col4.date_input("Enter Response Deadline", format="MM/DD/YYYY")
+type_of_subpoena = col5.text_input("Enter Subpoena Type")
